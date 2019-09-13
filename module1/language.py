ce08b16c511b7d79870fc7b5fc88e11615bfbbd9
madewild <madewild@ulb.ac.be>
2019-09-13 14:46:30 +0200

    add comment

--- module1/language.py
+++ module1/language.py
@@ -4,7 +4,7 @@ import os
 from collections import defaultdict
 
 import langid
-langid.set_languages(['fr', 'nl'])
+langid.set_languages(['fr', 'nl']) # restricting to these two languages only to prevent weird results
 
 lang_dict = defaultdict(int)
 
