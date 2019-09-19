a6aafae0be112e53800382720b372543ee2ba11f
madewild <madewild@ulb.ac.be>
2019-09-19 12:24:39 +0200

    add arguments

--- module1/sparql.py
+++ module1/sparql.py
@@ -1,9 +1,14 @@
 """Query Wikidata for Belgian politicians"""
 
+import argparse
 from datetime import datetime as dt
 import sys
 
 from SPARQLWrapper import SPARQLWrapper, JSON
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--filter', type=str, help='Filtering on name')
+parser.add_argument('-n', '--number', type=int, help='Number of rows to display')
 
 def get_rows():
     endpoint = "https://query.wikidata.org/bigdata/namespace/wdq/sparql"
@@ -11,11 +16,11 @@ def get_rows():
 
     statement = """
     SELECT DISTINCT ?person ?personLabel ?dateBirth ?dateDeath WHERE {
-    ?person wdt:P27 wd:Q31 .
-    ?person wdt:P106 wd:Q82955 .
-    ?person wdt:P569 ?dateBirth .
-    ?person wdt:P570 ?dateDeath .
-    SERVICE wikibase:label { bd:serviceParam wikibase:language "en" . }
+        ?person wdt:P27 wd:Q31 .
+        ?person wdt:P106 wd:Q82955 .
+        ?person wdt:P569 ?dateBirth .
+        ?person wdt:P570 ?dateDeath .
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en" . }
     }
     ORDER BY ?personLabel
     """
@@ -28,19 +33,28 @@ def get_rows():
     print(f"\n{len(rows)} Belgian politicians found\n")
     return rows
 
-def show(rows, n=10):
+def show(rows, filter=None, n=10):
     """Display n politicians (default=10)"""
     date_format = "%Y-%m-%dT%H:%M:%SZ"
+    if filter:
+        rows = [row for row in rows if filter in row['personLabel']['value'].lower()]
     print(f"Displaying the first {n}:\n")
     for row in rows[:n]:
-        birth_date = dt.strptime(row['dateBirth']['value'], date_format)
-        death_date = dt.strptime(row['dateDeath']['value'], date_format)
-        print(f"{row['personLabel']['value']} ({birth_date.year}-{death_date.year})")
+        try:
+            birth_date = dt.strptime(row['dateBirth']['value'], date_format)
+            birth_year = birth_date.year
+        except ValueError:
+            birth_year = "????"
+        try:
+            death_date = dt.strptime(row['dateDeath']['value'], date_format)
+            death_year = death_date.year
+        except ValueError:
+            death_year = "????"
+        print(f"{row['personLabel']['value']} ({birth_year}-{death_year})")
 
 if __name__ == "__main__":
+    args = parser.parse_args()
     rows = get_rows()
-    try:
-        n = int(sys.argv[1])
-    except IndexError:
-        n = 10
-    show(rows, n)
+    filter = args.filter if args.filter else None
+    number = args.number if args.number else 10
+    show(rows, filter, number)
