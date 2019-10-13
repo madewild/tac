64132331de22d55995a988e9cd49a2509788fcad
madewild <madewild@ulb.ac.be>
2019-10-13 22:40:01 +0200

    split dics

--- module2/explore.py
+++ module2/explore.py
@@ -8,6 +8,7 @@ import numpy as np
 
 path = "data/txt/"
 dic = defaultdict(int)
+dic2 = defaultdict(int)
 files = sorted(os.listdir(path))
 
 for f in files:
@@ -17,24 +18,23 @@ for f in files:
         year = elems[1]
         tome = elems[3]
         decade = year[:3] + "0"
-        dic[city] += 1
         dic[decade] += 1
-        dic[tome] += 1
-
+        dic2[city] += 1
+        dic2[tome] += 1
     else:
         print(f"Anomalous file: {f}")
 
-print(f"There are {dic['Bxl']} bulletins from Brussels and {dic['Lkn']} from Laeken")
-nb_rap = dic['RptAn']
+print(f"There are {dic2['Bxl']} bulletins from Brussels and {dic2['Lkn']} from Laeken")
+nb_rap = dic2['RptAn']
 print(f"{len(files)-nb_rap-1} are real bulletins and {nb_rap} are annual reports")
 
 def plot_bar():
     index = np.arange(len(dic))
     plt.bar(index, dic.values())
-    plt.xlabel('Décennie', fontsize=5)
-    plt.ylabel('# bulletins', fontsize=5)
-    plt.xticks(index, dic.keys(), fontsize=5, rotation=30)
+    plt.xlabel('Décennie')
+    plt.ylabel('# bulletins')
+    plt.xticks(index, dic.keys(), fontsize=8, rotation=30)
     plt.title('Évolution du nombre de bulletins')
     plt.show()
 
-#plot_bar()
+plot_bar()
