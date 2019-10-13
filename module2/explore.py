88377192a5dade22dbea6f28e845b49626b23729
madewild <madewild@ulb.ac.be>
2019-10-13 22:30:32 +0200

    add stats on cities and tomes

--- module2/explore.py
+++ module2/explore.py
@@ -9,15 +9,24 @@ import numpy as np
 path = "data/txt/"
 dic = defaultdict(int)
 files = sorted(os.listdir(path))
-print(files[:10])
 
 for f in files:
     if "_" in f:
-        year = f.split("_")[1]
+        elems = f.split("_")
+        city = elems[0]
+        year = elems[1]
+        tome = elems[3]
         decade = year[:3] + "0"
+        dic[city] += 1
         dic[decade] += 1
+        dic[tome] += 1
+
     else:
         print(f"Anomalous file: {f}")
+
+print(f"There are {dic['Bxl']} bulletins from Brussels and {dic['Lkn']} from Laeken")
+nb_rap = dic['RptAn']
+print(f"{len(files)-nb_rap-1} are real bulletins and {nb_rap} are annual reports")
 
 def plot_bar():
     index = np.arange(len(dic))
@@ -25,7 +34,7 @@ def plot_bar():
     plt.xlabel('Décennie', fontsize=5)
     plt.ylabel('# bulletins', fontsize=5)
     plt.xticks(index, dic.keys(), fontsize=5, rotation=30)
-    plt.title('Nombre de bulletins par décennie')
+    plt.title('Évolution du nombre de bulletins')
     plt.show()
 
-plot_bar()
+#plot_bar()
