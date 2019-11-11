f7f983969fee5155803edc059c35a31f8c44c7e0
madewild <madewild@ulb.ac.be>
2019-11-11 16:08:27 +0100

    play with w2v

---
+++ module4/model_explorer.py
@@ -0,0 +1,19 @@
+"""Playing with word2vec model"""
+
+from gensim.models import Word2Vec
+from pprint import pprint
+
+model = Word2Vec.load("data/bulletins.model")
+
+word1 = "boucher"
+word2 = "fleuriste"
+sim1 = model.wv.similarity(word1, word2)
+print(f"{word1} is {100*sim1:.1f}% similar to {word2}\n")
+word2 = "boulanger"
+sim1 = model.wv.similarity(word1, word2)
+print(f"{word1} is {100*sim1:.1f}% similar to {word2}\n")
+
+pprint(model.wv.most_similar("kermesse"))
+pprint(model.wv.most_similar("bruxelles"))
+
+pprint(model.most_similar(positive=['bruxelles', 'france'], negative=['belgique']))
