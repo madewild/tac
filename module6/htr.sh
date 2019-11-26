bc429376b2d62cc1227a22e6396ceda563b24396
madewild <madewild@ulb.ac.be>
2019-11-26 20:36:09 +0100

    warning

--- module6/htr.sh
+++ module6/htr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Installing SimpleHTR and testing handwritten text recognition
+# Installing SimpleHTR and testing handwritten text recognition (only works with <=3.6 !!!)
 
 git clone https://github.com/githubharald/SimpleHTR.git
 cd SimpleHTR/
