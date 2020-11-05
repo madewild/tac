d17f2ef0a60801e21d09c8340fe05454cf35c257
madewild <madewild@ulb.ac.be>
2020-11-05 08:28:50 +0100

    use filtering instead of filter

--- module3/s2_wordcloud.sh
+++ module3/s2_wordcloud.sh
@@ -4,6 +4,6 @@
 
 YEAR=$1
 cat data/txt/*_${YEAR}_*.txt > module3/${YEAR}.txt
-python module3/filter.py $YEAR
+python module3/filtering.py $YEAR
 wordcloud_cli --text module3/${YEAR}_keywords.txt --imagefile module3/${YEAR}.png --width 2000 --height 1000
 display module3/${YEAR}.png
