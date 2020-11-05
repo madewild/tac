fa0044af36f09ed6483035a4e5528ad3be7f4cc2
madewild <madewild@ulb.ac.be>
2020-11-05 16:08:01 +0100

    use -enc for explicit utf8 encoding

--- module2/s1_convert.sh
+++ module2/s1_convert.sh
@@ -2,7 +2,7 @@
 
 # Converting PDFs to text files and moving them to a new directory
 
-for file in data/pdf/*.pdf; do pdftotext "$file" "${file%.*}.txt"; done
+for file in data/pdf/*.pdf; do pdftotext -enc UTF-8 "$file" "${file%.*}.txt"; done
 mkdir data/txt
 mv data/pdf/*.txt data/txt/
 ls data/txt | wc -l
