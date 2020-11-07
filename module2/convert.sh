6e0e4e75cea3b303716a53b082bc0fa2129010ef
Ch. Katsidonis <ckatsidonis@gmail.com>
2020-11-07 17:03:48 +0100

    path

--- module2/convert.sh
+++ module2/convert.sh
@@ -2,9 +2,9 @@
 
 # Converting PDFs to text files and moving them to a new directory
 
-for file in data/pdf/*.pdf; do pdftotext "$file" "${file%.*}.txt"; done
-mkdir data/txt
-mv data/pdf/*.txt data/txt/
-ls data/txt | wc -l
-cat data/txt/*.txt > data/all.txt
-wc data/all.txt
+for file in /Volumes/Macintosh HD/Data/ADB/pdf/*.pdf; do pdftotext "$file" "${file%.*}.txt"; done
+mkdir /Volumes/Macintosh HD/Data/txt
+mv /Volumes/Macintosh HD/Data/ADB/pdf/pdf/*.txt /Volumes/Macintosh HD/Data/ADB/pdf/txt/
+ls /Volumes/Macintosh HD/Data/ADB/pdf/txt | wc -l
+cat /Volumes/Macintosh HD/Data/ADB/pdf/txt/*.txt > /Volumes/Macintosh HD/Data/ADB/pdf/all.txt
+wc /Volumes/Macintosh HD/Data/ADB/pdf/all.txt
