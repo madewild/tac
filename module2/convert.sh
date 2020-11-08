2c192cf889bbddf6704ebf040354a17cb3796892
Ch. Katsidonis <ckatsidonis@gmail.com>
2020-11-08 11:28:26 +0100

    Corrected paths

--- module2/convert.sh
+++ module2/convert.sh
@@ -3,8 +3,8 @@
 # Converting PDFs to text files and moving them to a new directory
 
 for file in /Volumes/Macintosh\ HD/Data/ADB/pdf/*.pdf; do pdftotext "$file" "${file%.*}.txt"; done
-mkdir /Volumes/Macintosh HD/Data/txt
-mv /Volumes/Macintosh HD/Data/ADB/pdf/pdf/*.txt /Volumes/Macintosh HD/Data/ADB/pdf/txt/
-ls /Volumes/Macintosh HD/Data/ADB/pdf/txt | wc -l
-cat /Volumes/Macintosh HD/Data/ADB/pdf/txt/*.txt > /Volumes/Macintosh HD/Data/ADB/pdf/all.txt
-wc /Volumes/Macintosh HD/Data/ADB/pdf/all.txt
+mkdir /Volumes/Macintosh\ HD/Data/txt
+mv /Volumes/Macintosh\ HD/Data/ADB/pdf/pdf/*.txt /Volumes/Macintosh\ HD/Data/ADB/pdf/txt/
+ls /Volumes/Macintosh\ HD/Data/ADB/pdf/txt | wc -l
+cat /Volumes/Macintosh\ HD/Data/ADB/pdf/txt/*.txt > /Volumes/Macintosh\ HD/Data/ADB/pdf/all.txt
+wc /Volumes/Macintosh\ HD/Data/ADB/pdf/all.txt
