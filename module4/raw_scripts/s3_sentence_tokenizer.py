0f8c9b04d29deece0c368d5004fb47f9a1dbb607
madewild <max.de.wilde@ulb.be>
2021-11-12 16:23:22 +0100

    cleaning up unused stuff

--- module4/raw_scripts/s3_sentence_tokenizer.py
+++ module4/raw_scripts/s3_sentence_tokenizer.py
@@ -1,14 +1,12 @@
 """Tokenize sentences"""
 
-import os
-import sys
 import nltk
 from nltk.tokenize import sent_tokenize
 
 nltk.data.path.append("/home/max/nltk_data")
 
-infile = f"data/all.txt"
-outfile = f"data/sents.txt"
+infile = "data/oneliner.txt"
+outfile = "data/sents.txt"
 
 with open(outfile, 'w', encoding="utf-8") as output:
     with open(infile, encoding="utf-8", errors="backslashreplace") as f:
